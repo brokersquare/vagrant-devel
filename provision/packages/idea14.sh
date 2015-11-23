7fd427a15549014710b2d9078e676cb719e8ca66
rucka <rucka@tiscalinet.it>
2015-11-23 09:40:00 +0100

    fixed wrong idea foder reference

--- provision/packages/idea14.sh
+++ provision/packages/idea14.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # download idea 14
-if [ -d "/home/vagrant/workspace/devel/idea/idea-IU-135.909" ]; then
+if [ -d "/home/vagrant/workspace/devel/idea/idea-IU-141.1532.4" ]; then
     echo "skip idea 14 installation"
 else
 	echo "idea 14 installation"
